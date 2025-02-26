2d9e4d66e6038542c581bdc0123b0574c791e6a8
Patrick Day <110992641+eldday@users.noreply.github.com>
2025-02-26 15:14:25 -0800

    Update db_connection.php

--- utilities/db_connection.php
+++ utilities/db_connection.php
@@ -1,7 +1,7 @@
 <?php
-$dbname = 'webresume';
-$username = 'pday';
-$password = 'quality';
+$dbname = '';
+$username = '';
+$password = '';
 
 try {
     $pdo = new PDO('mysql:dbname=' . $dbname . ';charset=utf8', $username, $password);
